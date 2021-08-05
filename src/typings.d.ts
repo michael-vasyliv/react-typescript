3552aa35ed850e0306b08736779bd70565c1b73a
Michael <mihail.vasyliv.mail@gmail.com>
2021-08-05 13:59:46 +0300

    feat: adding analyzer (#6)

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,8 +1,9 @@
 declare module '*.svg' {
     import * as React from 'react';
 
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+    const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
 
-    const src: string;
-    export default src;
+    export default ReactComponent;
 }
+
+type ExtendProps<T extends (...args: any) => any> = T extends (first: infer P) => any ? P : never;

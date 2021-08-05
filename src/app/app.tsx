dc1c9cf19ab04eaf4b55f868647b4e3e0c7d2a22
Michael Vasyliv <mihail.vasyliv.mail@gmail.com>
2021-08-05 13:57:50 +0300

    feat: adding analyzer

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import './app.scss';
-import logo from './logo.svg';
+import Logo from './logo.svg';
 
 const env = process.env.NODE_ENV?.toUpperCase();
 
 export function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                {env}
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <main className="main">
+            <Logo className="app-logo" />
+            {env}
+            <a
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </main>
     );
 }
